7759e0816210daab77545bdd7f925ccfe32394c9
gage <gvankuilenburg@mail.valenciacollege.edu>
2023-01-09 10:00:47 +0000

    Update 2 files

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Wallet from "./pages/Wallet";
 import Notifications from "./pages/Notifications";
-import { AppShell, useMantineTheme } from "@mantine/core";
+import {
+  AppShell,
+  useMantineTheme,
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
 import MantineHeader from "./Components/MantineHeader";
 import MantineNavBar from "./Components/MantineNavBar";
 import MantineFooter from "./Components/MantineFooter";
@@ -15,36 +21,50 @@ import MantineSideBar from "./Components/MantineSideBar";
 
 function App() {
   const theme = useMantineTheme();
-
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+    
   return (
-    <BrowserRouter>
-      <AppShell
-        styles={{
-          main: {
-            background:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        }}
-        navbarOffsetBreakpoint="sm"
-        asideOffsetBreakpoint="sm"
-        navbar={<MantineNavBar />}
-        aside={<MantineSideBar />}
-        footer={<MantineFooter />}
-        header={<MantineHeader />}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
       >
-        <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </AppShell>
-    </BrowserRouter>
+        <BrowserRouter>
+          <AppShell
+            styles={{
+              main: {
+                background:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[8]
+                    : theme.colors.gray[0],
+              },
+            }}
+            navbarOffsetBreakpoint="sm"
+            asideOffsetBreakpoint="sm"
+            navbar={<MantineNavBar />}
+            aside={<MantineSideBar />}
+            footer={<MantineFooter />}
+            header={<MantineHeader />}
+          >
+            <Routes>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/discover" element={<Discover />} />
+              <Route path="/wallet" element={<Wallet />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </AppShell>
+        </BrowserRouter>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
